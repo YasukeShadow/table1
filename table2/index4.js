b3c6c8c014341ce69d024cd22199d4b3280ebc03
YasukeShadow <freddev1976@gmail.com>
2022-04-24 12:51:35 +0200

    dossier de base test a jour au 24/04/2022

--- table2/index4.js
+++ table2/index4.js
@@ -21,7 +21,7 @@
     const adress = document.getElementById('adress');
     const statut = document.getElementById('statut');
     const signal = document.getElementById('signal');
-
+    
     const form =document.querySelector('form');
 
 
@@ -56,3 +56,54 @@
                 .then(()=> console.log("data ajouté"));
         
     })
+
+
+    ///////////////
+    // 
+    const tbody = document.querySelector('tbody');
+    let object
+    
+    fetch ("http://localhost:3000/dataUser") // alller chercher la bdd json 
+        .then( (data) => data.json())// 
+        .then( (result) => object = result) 
+        .then( (objectResult) => 
+                {
+                    //console.log(objectResult[0].nom);
+
+                    const createAllTable = document.getElementById("createAllTable");
+                    // const tr = document.createElement("tr");
+                    // const td = document.createElement("td");
+                    // tbody.appendChild(tr);
+                    // tr.appendChild(td);
+                    //  let textTable = document.createTextNode;
+                    //  td.appendChild(textTable);
+                    //console.log(objectResult);
+                    createAllTable.addEventListener("click", () =>
+                            {
+                               
+                                //td.appendChild(textTable);
+                                //console.log(objectResult) 
+                                for ( let i = 0; i <objectResult.length; i++ )
+                                {
+                                    const tr = document.createElement("tr");
+                                    tbody.appendChild(tr);
+
+                                    for (let j = 0 ; j<objectResult.length ; j++)
+                                        {
+                                            const td = document.createElement("td");
+                                            tr.appendChild(td);
+
+                                            td.foreach()
+                                            const tableText = document.createTextNode(objetResult);
+                                            td.appendChild(tableText);
+                                            tableText.textContent = objectResult[j].foreach()
+
+                                        }
+
+                                    console.log(objectResult[i].nom)
+                                }
+                            })
+                });
+
+
+        //console.log(object);
